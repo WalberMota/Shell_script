45dbe14307abf827978138d54d2aa0934586af4e
WalberMota <mota.csharp@gmail.com>
2024-01-25 21:12:04 -0300

    update hardclock.sh

---
+++ hardclock/hardclock.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+#####################################################################
+#																	#
+# Nome: notebook.sh													#
+#																	#
+# Autor: Walber Mota												#
+# Data : 16/12/2023													#
+#																	# 
+# Descrição: O script informa o modelo do processador, memória		#
+#			 disponível e espaço em disco.							#
+#																	#
+# Uso: ./notebook.sh												#
+#																	#
+#####################################################################
+
+CLKHARD=$(sudo hwclock --show)
+CLKSOFT=$(date -R)
+# CPUNO=$(cat /proc/cpuinfo | grep "model name"|wc -l)
+# CPUMODEL=$(cat /proc/cpuinfo | grep "model name"|head -n1|cut -c14-)
+# FILESYS=$(df -h|egrep -v '(tmpfs|udev|loop)')
+# MEMTOTAL=$(free -mh)
+# UPTIME=$(uptime -s)
+
+
+clear
+echo "======================================================================================"
+echo "Relatório para o usuário $USER"
+echo "Data/Hora: $(date)"
+echo "======================================================================================"
+echo "Inicio do trabalho atual: $UPTIME"
+echo
+echo "Versão do Kernel: $KERNEL"
+echo
+echo "CPUs:"
+echo "Quantidade de Cores: $CPUNO"
+echo "Modelo da CPU: $CPUMODEL"
+echo 
+echo "Memória Primária:"
+echo "$MEMTOTAL"
+echo
+echo "Memória Secundária:"
+echo "$FILESYS"
+echo
+echo "======================================================================================"
+
