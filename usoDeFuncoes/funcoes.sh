bc505c717c2ad29aef2299e0f845c184a8d3745d
WalberMota <mota.csharp@gmail.com>
2024-02-06 18:48:11 -0300

    created funcoes.sh

---
+++ usoDeFuncoes/funcoes.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "Mensagemfora da função"
+
+function mensagem1
+{
+    echo "Mensagem dentro da 1ª função"
+}
+
+function mensagem2()
+{
+    echo "Mensagem dentroa da 2ª função"
+}
+
+mensagem1
+mensagem2
+
