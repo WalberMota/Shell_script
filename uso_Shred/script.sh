a5a451be68e7e49f587953612e687a7fca4281b8
WalberMota <mota.csharp@gmail.com>
2024-02-06 18:04:48 -0300

    update script.sh

--- uso_Shred/script.sh
+++ uso_Shred/script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+# alinha acima é usada para  especificar qual interpretador do shell GNU será utilizado
+
 # o comando shred permite usar um arquivo lista como fazendo parte do meu script shell
 # util para criar funções e bibliotecas
 source variaveis
