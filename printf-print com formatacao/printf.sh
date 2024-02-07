7fd33bc7963ec6a00d53272c562c5ed8ea232bdd
WalberMota <mota.csharp@gmail.com>
2024-02-07 20:34:27 -0300

    update printf.sh

--- printf-print com formatacao/printf.sh
+++ printf-print com formatacao/printf.sh
@@ -6,6 +6,6 @@ printf "%s\n" "Mensagem 1" "Mensagem 2" "Mensagem 3" #aqui %s\n - dia que as cad
 
 printf "\n%s\n" "----Aviso 1" "----Aviso 2" "----Aviso 3" # uma quebra de linha antes e depois de cada cadeia de caracter
 
-printf "%s = %d\n" "Livro de Matematica" 230 "Livro de Portugues" 43 "Livro de Física" 120 #formatando com numero
+printf "%s = %d\n" "Livro de Matematica" 230 "Livro de Portugues" 43 "Livro de Astronomia" 120 #formatando com numero
 
-printf "%-25s:%d\n" "Livro de Matematica" 230 "Livro de Portugues" 43 "Livro de Física" 120
+printf "%-25s:%d\n" "Livro de Matematica" 230 "Livro de Portugues" 43 "Livro de Astronomia" 120
