e1747b00c1717e92f7988d69e1649f535d8c75c0
WalberMota <mota.csharp@gmail.com>
2024-02-07 20:20:37 -0300

    update printf.sh

--- printf-print com formatacao/printf.sh
+++ printf-print com formatacao/printf.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-printf "Mensagem !!!\nEstou em casa"    #com quebra de linha
+printf "Mensagem !!!\nEstou em casa\n"    #com quebra de linha
+
+printf "%s\n" "Mensagem 1" "Mensagem 2" "Mensagem 3" #aqui %s\n - dia que as cadeia de caracteres serão acrescidas de uma quebra de linha
+
+printf "\n%s\n" "----Aviso 1" "----Aviso 2" "----Aviso 3" # uma quebra de linha antes e depois de cada cadeia de caracter
