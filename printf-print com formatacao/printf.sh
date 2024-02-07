f1f98994a0d4ff605110df7e00864a4fd41a284f
WalberMota <mota.csharp@gmail.com>
2024-02-07 20:23:58 -0300

    update printf.sh

--- printf-print com formatacao/printf.sh
+++ printf-print com formatacao/printf.sh
@@ -5,3 +5,5 @@ printf "Mensagem !!!\nEstou em casa\n"    #com quebra de linha
 printf "%s\n" "Mensagem 1" "Mensagem 2" "Mensagem 3" #aqui %s\n - dia que as cadeia de caracteres serão acrescidas de uma quebra de linha
 
 printf "\n%s\n" "----Aviso 1" "----Aviso 2" "----Aviso 3" # uma quebra de linha antes e depois de cada cadeia de caracter
+
+printf "%s=%d" "Livro 1" 230 "Livro 2" 43 "Livro 3" 120
