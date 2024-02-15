a1b03f399c595b5a69fe4cfdc8d80da9bc7eebc8
Don’t Panic and Always Carry a Towel <74703752+WalberMota@users.noreply.github.com>
2024-02-15 17:33:27 -0300

    update relatoriosys.sh

--- RelatoriosDoSistema/relatoriosys.sh
+++ RelatoriosDoSistema/relatoriosys.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
 
-#####################################################################
-#																	#
-# Nome: notebook.sh													#
-#																	#
-# Autor: Walber Mota												#
-# Data : 16/12/2023													#
-#																	# 
-# Descrição: O script informa o modelo do processador, memória		#
-#			 disponível e espaço em disco.							#
-#																	#
-# Uso: ./notebook.sh												#
-#																	#
-#####################################################################
+#########################################################################
+#									                                    #
+# Nome: notebook.sh						                            	#
+#									                                    #
+# Autor: Walber Mota							                        #
+# Data : 16/12/2023 - Atualizado em 15/02/203			            	#
+#									                                    # 
+# Descrição: O script informa o modelo do processador, memória	    	#
+#			 disponível e espaço em disco.			                    #
+#									                                    #
+# Uso: ./notebook.sh							                        #
+#									                                    #
+#########################################################################
 
-KERNEL=$(uname -r)
+KERNEL=$(uname -a |egrep "Linux debian"| cut -c1-27)
 USER=$(whoami)
 CPUNO=$(cat /proc/cpuinfo | grep "model name"|wc -l)
 CPUMODEL=$(cat /proc/cpuinfo | grep "model name"|head -n1|cut -c14-)
 FILESYS=$(df -h|egrep -v '(tmpfs|udev|loop)')
 MEMTOTAL=$(free -mh)
 UPTIME=$(uptime -s)
+SENSORES=$(sensors)
 
 
 clear
-echo "======================================================================================"
+echo "========================================================================"
 echo "Relatório para o usuário $USER"
 echo "Data/Hora: $(date)"
-echo "======================================================================================"
+echo "========================================================================"
 echo "Inicio do trabalho atual: $UPTIME"
 echo
 echo "Versão do Kernel: $KERNEL"
@@ -42,5 +43,10 @@ echo
 echo "Memória Secundária:"
 echo "$FILESYS"
 echo
-echo "======================================================================================"
+echo "Temperatura:"
+echo "$SENSORES"
+echo "========================================================================"
+echo "qualquer tecla para sair..."
+read
+
 
